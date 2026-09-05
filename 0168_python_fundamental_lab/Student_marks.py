df23176ef73fbbbedf87d1d0e2aafadddd2902aa
Dolly-Verma12 <dollyverma2111@gmail.com>
2026-09-05 11:47:56 +0530

    Add Student marks file

---
+++ 0168_python_fundamental_lab/Student_marks.py
@@ -0,0 +1,12 @@
+stuname=input("Enter your name: ")
+Python=int(input("Enter Python marks: "))
+Database=int(input("Enter Database marks: "))
+Programming_Fundamentals=int(input("Enter Programming Fundamentals marks: "))
+Total =Python+Database+Programming_Fundamentals
+Average =Total/3
+print()
+print("===== MARKS REPORT =====")
+print("Student name :",stuname)
+print("Pyhton       :",Python)
+print("Database     :",Database)
+print("Programming  :",Programming_Fundamentals)
