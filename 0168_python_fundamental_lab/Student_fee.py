c41b87484adc4a20841691ba6aa6d19d76183d65
Dolly-Verma12 <dollyverma2111@gmail.com>
2026-09-05 11:48:54 +0530

    Add Student fee file

---
+++ 0168_python_fundamental_lab/Student_fee.py
@@ -0,0 +1,9 @@
+Student_name=input("Enter you Name: ")
+Course_fee=float(input("Enter your Course Fee: "))
+Registration_fee=float(input("Enter your Registration Fee: "))
+total=Course_fee+Registration_fee
+print()
+print("Course fee       :",Course_fee)
+print("Registration fee :",Registration_fee)
+print("-------------------------------------")
+print("Total : ",total)
