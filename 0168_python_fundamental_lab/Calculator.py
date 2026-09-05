2a2646154904f9456468d345c31fd2065fc6488d
Dolly-Verma12 <dollyverma2111@gmail.com>
2026-09-05 11:45:22 +0530

    hendle error

---
+++ 0168_python_fundamental_lab/Calculator.py
@@ -0,0 +1,16 @@
+Num1=int(input("Enter First Number: "))
+op=input("Enter Operator: ")
+Num2=int(input("Enter Second Number: "))
+print()
+if(op=='+'):
+    print("Result: ",Num1+Num2)
+elif(op=='-'):
+    print("Result: ",Num1-Num2)
+elif(op=='*'):
+    print("Result: ",Num1*Num2)
+elif(op=='/'):  
+     if Num2 == 0:    
+            raise ZeroDivisionError("Cannot divide by zero.")     
+     print("Result: ",Num1/Num2)
+else:
+    print("Invalid Operator!!")
