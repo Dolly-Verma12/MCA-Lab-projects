3c3f1cce20f57964b7d9985f82e6a06ed047eba5
Dolly-Verma12 <dollyverma2111@gmail.com>
2026-09-16 12:14:05 +0530

    update

--- 2628mca0168_Control_Structures_and_Program_Logig _lab/2628mca0168_multiple_product.py
+++ 2628mca0168_Control_Structures_and_Program_Logig _lab/2628mca0168_multiple_product.py
@@ -1,11 +1,8 @@
 number_of_products = int(input("Enter number of products: "))
-
 total = 0
-
 for i in range(number_of_products):
     price = float(input(f"Enter price of product {i + 1}: ₹"))
     total += price
-
 if total < 1000:
     discount_rate = 0
 elif total < 5000:
@@ -14,10 +11,8 @@ elif total < 10000:
     discount_rate = 10
 else:
     discount_rate = 15
-
 discount_amount = total * discount_rate / 100
 final_amount = total - discount_amount
-
 print("\n--- BILL SUMMARY ---")
 print("Total Amount: ₹", total)
 print("Discount:", discount_rate, "%")
